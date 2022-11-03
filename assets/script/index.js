72d16680f35622da0d13677d183f38d5269528c1
Lei Sun <139188566@qq.com>
2022-11-03 10:07:26 -0500

    add index.html and its css,js file

---
+++ assets/script/index.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+const inputShowRandom = document.querySelector(".input-cls");
+const btnGenerate = document.querySelector(".btn-compute");
+  
+function getRandomNum()
+{
+    return Math.floor(Math.random() * 50 + 1);
+}
+btnGenerate.onclick = function () {
+
+    inputShowRandom.value = getRandomNum();
+}
+ 
