f525ae012508101c945414865eab5a45248286d8
Lei Sun <139188566@qq.com>
2022-11-03 11:57:34 -0500

    Add logic

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,22 @@
 ﻿'use strict';
-const inputShowRandom = document.querySelector(".input-cls");
-const btnGenerate = document.querySelector(".btn-compute");
+const guess = document.querySelector(".guess");
+const newNum = document.querySelector(".sys-number");
+const msg = document.querySelector(".msg-cls");
+const btn = document.querySelector(".btn-compute");
   
 function getRandomNum()
 {
     return Math.floor(Math.random() * 50 + 1);
 }
-btnGenerate.onclick = function () {
+btn.onclick = function () {
 
-    inputShowRandom.value = getRandomNum();
+    var myNumber = getRandomNum();
+    var sysNumber = getRandomNum();
+    guess.innerText = `Guesses: ${myNumber}`;
+    newNum.innerText = `${sysNumber}`;
+
+    myNumber > sysNumber ? msg.innerText = "My number is biggger" : msg.innerText = "My number is smaller";
+
+    btn.value = "Restart";
 }
  
